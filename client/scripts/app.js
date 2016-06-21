5d9b9a386704a96a9f624eb0c17138d76bdbcdec
daredia <shehzad.daredia@gmail.com>
2016-06-21 10:24:15 -0700

    Implement add friend functionality and styling for existing and new messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,27 +7,6 @@
 // for...of loops (a new for loop, what!!)
 // template strings
 
-// object named app
-// app.init 
-  // initiate the app
-// app.send(message)
-  // submit a POST request with the message obj via $.ajax;
-// app.fetch()
-  // submit a GET request via $.ajax;
-// app.clearMessages() 
-  // clear messages from DOM (clear #chats)
-// app.addMessage(message) 
-  // add messages to DOM (to #chats)
-  // use jQuery to append messages in proper format
-// app.addRoom
-  // add rooms to DOM; use #roomSelect
-// app.addFriend
-  // see messages just from friend if username is clicked
-// app.handleSubmit
-  // send a message on click
-  // needs a submit button with a form for message
-
-
 // message is in object form
 // ex:  var message = {
         //   username: 'Mel Brooks',
@@ -44,14 +23,20 @@ app.init = () => {
 };
 
 app._lastShown = 0;
+app._friends = {};
 
 //take in data from .fetch and append to DOM
 app.addMessage = (messageObj) => {
   var $message = $('<div class=message></div>');
   //TODO: THINK ABOUT THIS. for new messages, check for username class, if 
+  // create a friend storage object, search for username in friend object, if true, then add .friend to it
+  // but for old messages, we will have to do a one-time loop everytime a new friend is added. 
+  // another option is creating a customized hash-function-esque numerical friend ID and setting that as a class? 
   $message.data('user', messageObj.username);
-  console.log('$message.data(user)', $message.data('user'));
   var $user = $('<span class=username></span>');
+  if (app._friends[messageObj.username]) {
+    $message.addClass('friend');
+  }
   $user.text(messageObj.username);
   $message.text(': ' + messageObj.text);
   $message.prepend($user); 
@@ -72,18 +57,20 @@ app.fetch = () => {
     url: app.server,
     type: 'GET',
     success: (data) => {
+      // return an array of new messages;
       var newMessages = _.filter(data.results, (message) => {
         return Date.parse(message.createdAt) > app._lastShown;
-      }); // return an array of new messages;
+      }); 
 
+      // loop through new messages and add them to DOM
       for (var messageObj of newMessages) {
         app.addMessage(messageObj);
       }
-      
+
+      // setting new time to compare against if there is a new message
       if (newMessages[0]) {
         app._lastShown = Date.parse(newMessages[0].createdAt);
       }
-      // setting new timeStamp if there is a new message
     },
     error: () => {
       console.log('error on fetch');
@@ -126,25 +113,40 @@ app.addRoom = (roomname) => {
 };
 
 app.addFriend = (node) => {
+  var username = node.text();
+  console.log('username in addFriend', username);
+  if (!app._friends[username]) {
+    app._friends[username] = true;
+    var $spans = $('span:contains("' + username + '")');
+    $spans.parent().addClass('friend');
+    console.log($spans.parent());
+  }
+  
   $(node).parent().addClass('friend');
   $(node).parent(); // get username and update the css of it
 };
 
 
 $(document).ready(function() {
+
+// Load chatroom's messages
   app.init();
+
+// can probably move this into app.init(). it just fetches every 2 seconds.
   setInterval(app.fetch, 2000);
+
+// Load new messages on click
   $('#load').on('click', app.fetch);
 
+
+// For submitting the message form
   $('#send').on('submit', (event) => {
     event.preventDefault();
     app.handleSubmit();
   });
 
-
 // DON'T USE ES6 FOR THIS, IT PRESERVES THE THIS BINDING TO THE DOCUMENT
   $('#chats').on('click', '.username', function() {
-    console.log($(this));
     app.addFriend($(this));    
   });
 });
