463fb426c5e17ef6a7f1b4bac7d81450e8753885
daredia <shehzad.daredia@gmail.com>
2016-06-21 17:10:02 -0700

    Refactor and make messages fade in and out

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,32 +1,55 @@
-// // YOUR CODE HERE:
-// Use ES6
-// arrow functions
-// spread (...) operator
-// rest parameters
-// object literal extensions
-// for...of loops (a new for loop, what!!)
-// template strings
-
-// message is in object form
-// ex:  var message = {
-        //   username: 'Mel Brooks',
-        //   text: 'It\'s good to be the king',
-        //   roomname: 'lobby'
-        // };
 
 let app = {};
 app.server = 'https://api.parse.com/1/classes/messages';
-
-
-
 app._lastShown = 0;
 app._friends = {};
 app._rooms = {};
 app._currentRoom = 'All Rooms';
 
+// for making custom Parse query with constraint, returning objects matching roomname
+// var suffix = '?where={"roomname": "secretHacker"}';
+
+// $.ajax({
+//   url: app.server + suffix,
+//   // hardcode
+//   type: 'GET',
+//   contentType: 'application/JSON',
+//   success: (data) => {
+//     console.log(data);
+//   },
+//   error: () => {
+//     console.log('failed constraint query');
+//   }
+// });
+
+
+app.init = () => {
+  //initiate the app with loaded messages
+  app.fetch();
+
+  setInterval(app.fetch, 2000);
+
+  // Load new messages on click
+  $('#load').on('click', app.fetch);
+
+  // For submitting the message form
+  $('#send').on('submit', (event) => {
+    event.preventDefault();
+    app.handleSubmit();
+  });
+
+  // DON'T USE ES6 FOR THIS, IT PRESERVES THE THIS BINDING TO THE DOCUMENT
+  $('#chats').on('click', '.username', function() {
+    app.addFriend($(this));    
+  });
+
+  //listener for RoomSelect
+  $('#roomSelect').on('change', app.changeRoom);
+};
+
 //take in data from .fetch and append to DOM
 app.addMessage = (messageObj) => {
-  var $message = $('<div class=message></div>');
+  var $message = $('<div class="message"></div>');
   
   // escaping XSS
   for (var prop in messageObj) {
@@ -37,11 +60,12 @@ app.addMessage = (messageObj) => {
   if (!app._rooms[messageObj.roomname]) {
     app.addRoom(messageObj.roomname);
   }
-  // TO FIX: THIS IS BREAKING IF NO ROOMNAME
+
+  messageObj.roomname = messageObj.roomname || 'All Rooms';
   $message.addClass(messageObj.roomname.split(' ').join('_'));
 
   // adding username
-  var $user = $('<span class=username></span>');
+  var $user = $('<span class="username"></span>');
   $user.text(messageObj.username);
 
   // testing for friend & adding .friend
@@ -54,12 +78,17 @@ app.addMessage = (messageObj) => {
 
   $message.prepend($user); 
   
+  $message.css('display', 'none');
+
   // deciding order for message to be added
   if (app._lastShown) {
-    $('#chats').prepend($message);  
+    $('#chats').prepend($message);
+    console.log('$message', $message);
   } else {
-    $('#chats').append($message);  
+    $('#chats').append($message);
   }
+
+  $message.fadeIn(800);
 
 
   // filter for roomName, if it doesn't match the currentRoom & it's not all Rooms, then hide;
@@ -77,6 +106,7 @@ app.fetch = () => {
   $.ajax({
     url: app.server,
     type: 'GET',
+    contentType: 'application/JSON',
     success: (data) => {
       // return an array of new messages;
       var newMessages = _.filter(data.results, (message) => {
@@ -111,6 +141,9 @@ app.send = (message) => {
       app.fetch();
       // draw new messages from parse server; 
       // keep track of messages that are in the DOM and only append new ones
+    },
+    error: () => {
+      console.error('send failed');
     }
   });
 };
@@ -156,47 +189,10 @@ app.changeRoom = () => {
   }
 
   if (app._currentRoom === 'All Rooms') {
-    $('#chats').children().show();
+    $('#chats').children().hide().fadeIn(800);
   } else {
     $('#chats').children().hide();
-    var classname = '.' + app._currentRoom;
-    classname = classname.split(' ').join('_');
-    var $roomShow = $(classname);
-    $roomShow.show(); // is show not working?
+    var $roomShow = $('.' + app._currentRoom.split(' ').join('_'));
+    $roomShow.fadeIn(800); 
   }
 };
-
-app.init = () => {
-  //initiate the app with loaded messages
-  app.fetch();
-
-  setInterval(app.fetch, 2000);
-
-  // Load new messages on click
-  $('#load').on('click', app.fetch);
-
-  // For submitting the message form
-  $('#send').on('submit', (event) => {
-    event.preventDefault();
-    app.handleSubmit();
-  });
-
-  // DON'T USE ES6 FOR THIS, IT PRESERVES THE THIS BINDING TO THE DOCUMENT
-  $('#chats').on('click', '.username', function() {
-    app.addFriend($(this));    
-  });
-
-  //listener for RoomSelect
-  $('#roomSelect').on('change', app.changeRoom);
-};
-
-$(document).ready(function() {
-// Load chatroom & event listeners
-  app.init();
-});
-
-
-
-
-
-
